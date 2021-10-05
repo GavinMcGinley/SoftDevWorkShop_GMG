88baf12246cc40dd9a77af8ce9ea666005e136a8
GavinMcGinley <gavinjhmcginley@gmail.com>
2021-10-05 16:20:27 -0400

    uploaded group's files

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -1,51 +1,25 @@
-"""#Gavin McGinley
-#SoftDev
-#k10-- trying out flask
-#2021--10--04
-#SUMMARY OF DISCUSSION
-We mainly just tried to get flask to work and pulled the required files from git"""
+"""
+Cool Dogs - Ishraq Mahid & Fish, Ivan Mijacika & Bob, and Gavin McGinley & Craig
+SoftDev
+k10 -- Flask Greatness -- Exploration of Flask framework -- We explored written code to understand flask and created our own code
+2021 - 10 - 4
+"""
 from flask import Flask
-app = Flask(__name__) #create instance of class Flask
+from randomOcc06 import weighted
+from randomOcc06 import generateListStr
 
-def weighted():
-    import csv
-    import random
+app = Flask(__name__)  # create instance of class Flask
+tnpg = "Cool Dogs - Ishraq Mahid & Fish, Ivan Mijacika & Bob, and Gavin McGinley & Craig"
 
-    #Reads CSV into program as "reader"
-    csv_file=open("occupations.csv")
-    reader=csv.reader(csv_file)
+@app.route("/")  # assign fxn to route
 
-    #skips first line
-    next(reader)
-
-    #Adds CSV info to dictionary
-    wdict={}
-    for row in reader:
-        wdict[row[0]]=float(row[1])
-
-    #Randomly generates number
-    num=random.randint(1,998)
-    total=0;
-
-    #Likeliness of passing total is proportional to its percentage
-    for x in wdict.keys():
-        total=total+(wdict[x]*10)
-        if total>=num:
-            return(x)
-            break
-
-@app.route("/")       #assign fxn to route
 def hello_world():
-    tmpg="The Cool Dogs: Bobby, Craig, Fish, Ishraq Mahid, Ivan Mijacika, Gavin McGinley"
+    #This code does similar to what is done in v3, with the exception of dealing with cases where the file is imported somewhere else
+    
+    return tnpg + "<br>" + 'The selected occupation is: ' + weighted() + "<br>" + generateListStr()
 
-    q= "Chosen Occupation: " + weighted()
-
-    q= tmpg +"\n"+q
-    return q
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
-
-
 
